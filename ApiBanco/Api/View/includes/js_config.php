57ac2cad25e637a254f145fd66c1acad2962fe35
Rodrigo Videira <102252756+diginxz@users.noreply.github.com>
2023-04-25 14:53:15 -0300

    Delete ApiBanco/Api/View directory

--- ApiBanco/Api/View/includes/js_config.php
+++
@@ -1,6 +0,0 @@
-﻿<!--
-    Este é um arquivo responsável por armazenar itens relacionados ao js da
-    sua aplicação, seja ele links de frameworks e muito mais.
--->
-
-<script src="./../../View/bootstrap/js/bootstrap.bundle.min.js"></script>
