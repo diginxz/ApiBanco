f35ecd42da54dc6e3c84a4b89e7eb5cd3770e076
Rodrigo Videira <102252756+diginxz@users.noreply.github.com>
2023-07-25 14:08:50 -0300

    Update CorrentistaModel.php

--- ApiBanco/Api/Model/CorrentistaModel.php
+++ ApiBanco/Api/Model/CorrentistaModel.php
@@ -2,26 +2,39 @@
 
 namespace API\Model;
 
+use API\DAO\CorrentistaDAO;
+
 class CorrentistaModel extends Model {
 	public $id, $nome, $cpf, $data_nasc, $senha;
 
-	public function save() 
+	public function save()
 	{
-
+		return (new CorrentistaDAO())->save($this);
 	}
+
+	public function getByCpfAndSenha($cpf, $senha) : CorrentistaModel
+    {      
+        return (new CorrentistaDAO())->selectByCpfAndSenha($cpf, $senha);
+    }
 
 	public function getAllRows() 
 	{
+		$dao = new CorrentistaDAO();
 
+		$this->rows = $dao->select();
 	}
 
 	public function delete(int $id) 
 	{
-
+		$dao = new CorrentistaDAO();
+		
+		$dao->delete($id);
 	}
 
 	public function getById(int $id) 
 	{
-        
+		$dao = new CorrentistaDAO();
+
+		$this->rows = $dao->selectById($id);
 	}
 }
